02b70e0e71d186b10784797ea5f7904d19956d3d
0einstein0 <fzulfiqar.bscs18seecs@seecs.edu.pk>
2021-09-08 14:15:28 +0500

    carousel modal

--- js/index.js
+++ js/index.js
@@ -53,3 +53,47 @@ $('#prev').click(function() {
 $('#next').click(function() {
   moveToSelected('next');
 });
+
+
+
+
+
+
+
+
+function getId(id){
+  var modal = document.getElementById('myModal');
+  var imgId = id.id;
+  var vidSrc;
+  var img=document.getElementById(imgId);
+  var modalImg = document.getElementById("img01");
+  var modalVid = document.getElementById("vid01");
+  var captionText = document.getElementById("caption");
+  modal.style.display = "block";
+  console.log(img.tagName);
+    if(img.getAttribute('vidsrc')){
+      vidSrc = img.getAttribute('vidsrc');
+      modalImg.style.display = "none";
+      modalVid.style.display = "block";
+      modalVid.src = vidSrc;
+      modalVid.alt = img.alt;
+    
+    }
+    else{ 
+    modalVid.style.display = "none";
+    modalImg.style.display = "block";
+    modalImg.src = img.src;
+    modalImg.alt = img.alt;}
+ 
+  
+  captionText.innerHTML = img.alt;  
+
+  var span = document.getElementsByClassName("close")[0];
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+
+}
+
+
